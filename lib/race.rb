b2a2ef9b9ade991cf243699386592ee77547b72e
Robert Zehnder <robzcodes@gmail.com>
2016-10-27 12:01:02 -0400

    Finishes first two classes - all tests passing

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,36 @@
 class Race
 
+  attr_accessor :name
+  attr_accessor :race_type
+  attr_accessor :distance
+  attr_accessor :km_times
+
+  def initialize(name,race_type,distance,km_times)
+    @name = name
+    @race_type = race_type
+    @distance = distance
+    @km_times = km_times
+  end
+
+  def total_time
+    sum=0
+    km_times.each do |time|
+      sum = sum + time
+    end
+    return sum
+  end
+
+  def slowest_km
+    return km_times.max
+  end
+
+  def quickest_km
+    return km_times.min
+  end
+
+  def avg_time_per_km
+    avgTime = km_times.inject{ |sum, el| sum + el }.to_f / km_times.size
+    return avgTime.round(2)
+  end
+
 end
--- lib/racer.rb
+++ lib/racer.rb
@@ -1,3 +1,32 @@
 class Racer
+  attr_accessor :name
+  attr_reader :races
+
+  def initialize(name)
+    @name = name
+    @races = []
+  end
+
+  def add_race(race)
+    @races.push(race)
+  end
+
+  def best_race(type,distance)
+    quickTime=0
+    theRace =""
+    @races.each do |x|
+      if x.race_type == type && x.distance == distance
+        if quickTime == 0
+          quickTime = x.total_time
+          theRace = x
+        elsif x.quickest_km < quickTime
+          quickTime = x.total_time
+          theRace = x
+        end
+      end
+    end
+    return theRace
+  end
+
 
 end
